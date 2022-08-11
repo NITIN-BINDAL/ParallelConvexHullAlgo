c11bb1184590e9265088cb0d78c9d924a9138dca
Nitin Bindal <nitinbindal100@gmail.com>
2022-08-12 01:52:46 +0530

    Updated Readme and Organized files

--- src/ex.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-g++ -std=c++2a Sequential.cpp
-./a.out<input.txt>SequentialOutput.txt
-g++ -std=c++2a Parallelized.cpp -fopenmp
-./a.out<input.txt>ParallelizedOutput.txt
-cmp --silent output1.txt output2.txt && echo '### SUCCESS: Files Are Identical! ###' || echo '### WARNING: Files Are Different! ###' 
